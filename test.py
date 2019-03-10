a4434f4edcf4481b68dbfaf48dd690362ede402a
Jeremiah Nelson <miahnelson@gmail.com>
2019-03-10 18:31:30 -0500

    Added to readme and added images

--- test.py
+++ test.py
@@ -16,5 +16,5 @@ if __name__ == '__main__':
     thread = threading.Thread(target=start_server)
     thread.daemon = True
     thread.start()
-    webview.create_window("This is just a test", "http://127.0.0.1:50000/")
+    webview.create_window("This is just a test", "http://127.0.0.1:5000/")
     sys.exit()
